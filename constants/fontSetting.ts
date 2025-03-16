d7e5bc4138031233519cf3eaf109b6756c217da4
NSMatthew <matthewnovansidharta31@gmail.com>
2025-03-16 18:34:00 +0800

    setting font type, size, dan color

---
+++ constants/fontSetting.ts
@@ -0,0 +1,16 @@
+export const fontType = {
+Regular: 'Satoshi-Regular',
+Medium: 'Satoshi-Medium',
+Bold: 'Satoshi-Bold',
+};
+
+export const fontSize = {
+Regular: 12,
+Medium: 16,
+Bold: 20,
+};
+
+export const fontColor = {
+primary: '#0E0E0E',
+secondary: '#5B5E6B',
+};
