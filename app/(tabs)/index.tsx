53b028099f1da69b70e35d5e74a40f5344ab8554
NSMatthew <matthewnovansidharta31@gmail.com>
2025-03-18 11:09:05 +0800

    setting navigasi splash ke home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Stack } from 'expo-router';
+import { Stack } from 'expo-router';  // Menggunakan Stack untuk navigasi layar
 
 export default function Layout() {
   return (
     <Stack>
+      {/* Splash screen sebagai layar pertama */}
       <Stack.Screen 
-        name="Splash" 
-        options={{ headerShown: false }} 
+        name="Splash" // Pastikan nama sesuai dengan file SplashScreen.tsx
+        options={{ headerShown: false }} // Menyembunyikan header
       />
+      {/* Home screen setelah SplashScreen */}
       <Stack.Screen 
-        name="Home" 
-        options={{ headerShown: false }} 
+        name="Home" // Pastikan nama sesuai dengan file Home.tsx
+        options={{ headerShown: false }} // Menyembunyikan header
       />
     </Stack>
   );
