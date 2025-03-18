4f76a92d65a521eecbc67768bc0b6e889a1ff0cc
NSMatthew <matthewnovansidharta31@gmail.com>
2025-03-18 16:45:20 +0800

    ngubah spacing di layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,12 +4,10 @@ import { RootStackParamList } from '../Navigation/types';  // Import the type
 export default function Layout() {
   return (
     <Stack initialRouteName="Splash">  {/* Set SplashScreen as the first screen */}
-      <Stack.Screen 
-        name="Splash"  // Define SplashScreen as a route (no need for "/Screen/...")
+      <Stack.Screen name="Splash"  // Define SplashScreen as a route (no need for "/Screen/...")
         options={{ headerShown: false }}  // Hide the header for SplashScreen
       />
-      <Stack.Screen 
-        name="Home"  // Define Home as a route (no need for "/Screen/...")
+      <Stack.Screen name="Home"  // Define Home as a route (no need for "/Screen/...")
         options={{ headerShown: false }}  // Hide the header for HomeScreen
       />
     </Stack>
