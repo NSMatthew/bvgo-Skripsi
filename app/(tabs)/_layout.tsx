fef64ece0db9be26bc9d7385852a600dfbcdbd7d
NSMatthew <matthewnovansidharta31@gmail.com>
2025-03-18 10:52:52 +0800

    setting layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,17 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Stack } from 'expo-router'; // Import Stack untuk navigasi antar screen
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
+    <Stack>
+      <Stack.Screen 
+        name="Splash" // SplashScreen akan ditampilkan pertama kali
+        options={{ headerShown: false }}  // Menyembunyikan header pada SplashScreen
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
+      <Stack.Screen 
+        name="Home" // Setelah SplashScreen, akan menuju HomeScreen
+        options={{ headerShown: false }}  // Menyembunyikan header pada HomeScreen
       />
-    </Tabs>
+    </Stack>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,8 +4,14 @@ import { Stack } from 'expo-router';
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="Splash" options={{ headerShown: false }} />
-      <Stack.Screen name="Home" options={{ headerShown: false }} />
+      <Stack.Screen 
+        name="Splash" 
+        options={{ headerShown: false }} 
+      />
+      <Stack.Screen 
+        name="Home" 
+        options={{ headerShown: false }} 
+      />
     </Stack>
   );
 }
