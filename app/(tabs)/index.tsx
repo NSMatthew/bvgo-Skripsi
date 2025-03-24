0a0e29b990c0d294ebbb1376b6682b0741895468
NSMatthew <matthewnovansidharta31@gmail.com>
2025-03-24 17:09:23 +0800

    revert index.tsx untuk mencegah error

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,41 +1,23 @@
-import React, { useEffect, useRef } from "react";
-import { View, Text, Image, Animated } from "react-native";
-import { useRouter } from "expo-router";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { styles } from "../styles";
+import React from 'react';
+import { Redirect, Stack } from 'expo-router';
 
 export default function Index() {
-  const router = useRouter();
-  const fadeAnim = useRef(new Animated.Value(0)).current;
-
-  useEffect(() => {
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
-
-    setTimeout(async () => {
-      const token = await AsyncStorage.getItem("userToken");
-      if (token) {
-        router.replace("/(tabs)/Home");
-      } else {
-        router.replace("../login");
-      }
-    }, 2000);
-  }, []);
-
   return (
-    <Animated.View style={[styles.container, { opacity: fadeAnim }]}>
-      <Image
-        source={require("../assets/images/logo-bvgo.png")}
-        style={styles.logoBVGO}
-      />
-      <Text style={styles.splashText}>from</Text>
-      <Image
-        source={require("../assets/images/logo-bukitvista.png")}
-        style={styles.logoBV}
-      />
-    </Animated.View>
+    <>
+      {/* Redirect ensures the app navigates to the SplashScreen first */}
+      <Redirect href="../Screen/Splash" />
+
+      {/* Stack Navigator to handle the navigation between screens */}
+      <Stack initialRouteName="Splash">
+        <Stack.Screen
+          name="Splash"
+          options={{ headerShown: false }} // Hide header for Splash screen
+        />
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }} // Hide header for Home screen
+        />
+      </Stack>
+    </>
   );
 }
